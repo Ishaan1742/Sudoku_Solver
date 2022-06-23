d598ef7b8d14ae28d65e5762189bf17ef384cb37
Ishaan Agarwal <ishaan.ag1742@gmail.com>
2022-06-23 18:02:06 +0530

    Added support for online sudoku on sudoku.com

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ import time
 grid = getInput()
 greedyBacktrack(grid) #call the greedy backtrack function, grid is now a solved grid.
 
-#convert grid to chararcter array
+#convert grid to chararcter array for sending to sudoku.com as pyautogui works with characters
 grid = ConvertToChar(grid)
 
 time.sleep(5) #click on the grid (manually)
