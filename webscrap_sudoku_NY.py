3a95d9da79fa838351342dd7a524a96261f0f0e3
Ishaan Agarwal <ishaan.ag1742@gmail.com>
2022-06-27 08:35:33 +0530

    Final Changes

--- webscrap_sudoku_NY.py
+++ webscrap_sudoku_NY.py
@@ -42,7 +42,7 @@ def direct_enter(grid):
     EnterBoard(grid) # enter the grid into the website
 
 #time.sleep(1) # wait for 1 second
-#greedyBacktrack_visualiser(grid) # visualise the backtrack
-direct_enter(grid) # enter the grid into the website
+greedyBacktrack_visualiser(grid) # visualise the backtrack
+#direct_enter(grid) # enter the grid into the website
 
 
